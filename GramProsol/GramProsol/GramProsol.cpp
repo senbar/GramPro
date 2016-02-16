798b3699a95dfd8d2601ed9fbf9b9be610760592
senbar <barsen97@gmail.com>
2016-02-16 15:39:08 +0100

    dont know what im commiting

--- GramProsol/GramProsol/GramProsol.cpp
+++ GramProsol/GramProsol/GramProsol.cpp
@@ -17,6 +17,7 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -58,10 +59,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 
 
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
@@ -75,29 +72,20 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hInstance      = hInstance;
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GRAMPROSOL));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_GRAMPROSOL);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW);
+    wcex.lpszMenuName   = NULL;
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     return RegisterClassExW(&wcex);
 }
 
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
+
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+   hInst = hInstance; 
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
@@ -111,45 +99,61 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE:  Processes messages for the main window.
-//
-//  WM_COMMAND  - process the application menu
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
+	case WM_CREATE:
+		g_hwndPlot = CreateWindowExW(NULL , L"STATIC", L"test", WS_CHILD | WS_VISIBLE | WS_BORDER, 0, 0, CHART_WIDTH, CHART_HEIGHT+3, hWnd, (HMENU)IDC_STATIC, hInst, NULL);
+		if (g_hwndPlot==NULL)
+		{
+			MessageBoxW(NULL, L"Error", L"Plot window creation failed", NULL);
+			SendMessage(hWnd, WM_DESTROY, NULL, NULL);
+		}
+		if (CreateWindowExW(NULL, L"EDIT", L"Equation edit", WS_CHILD | WS_VISIBLE | WS_BORDER, 450, 100, 100, 20, hWnd, (HMENU)IDD_EQUATION_DIALOGBAR, hInst, NULL)==NULL)
+		{
+			MessageBoxW(NULL, L"Error", L"Equation edit window creation failed", NULL);
+			SendMessage(hWnd, WM_DESTROY, NULL, NULL);
+		}
+	
+
+		g_cChart = new Chart(&QuadraticEquation, -1, 1);
+		g_vDots = g_cChart->getGrainVector(RESOLUTION);
+		break;
+
     case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
+
+		switch (LOWORD(wParam))
+		{
+		case IDD_EQUATION_DIALOGBAR:
+			MessageBoxW(NULL, L"Error", L"Equation edit window creation failed", NULL);
+			break;
+
+		}
+
         break;
     case WM_PAINT:
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
+            
             EndPaint(hWnd, &ps);
+
+
+
+			PAINTSTRUCT psPlot;
+			HDC hdcPlot = BeginPaint(g_hwndPlot, &psPlot);
+			
+
+			for (int i = 0; i < RESOLUTION; i++)
+			{
+				SetPixel(hdcPlot, g_vDots[i].X, RESOLUTION-g_vDots[i].Y, RGB(0, 0, 0));
+			}
+			SetPixel(hdcPlot, 200, 400, RGB(0, 255, 0));
+			EndPaint(g_hwndPlot, &psPlot);
         }
         break;
+	
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -159,22 +163,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-// Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+//
+//
+double QuadraticEquation(double x)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	
+	return (x*x*x);
 }
--- GramProsol/GramProsol/GramProsol.h
+++ GramProsol/GramProsol/GramProsol.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "resource.h"
+#include "cChart.h"
+#include <iostream>
+HWND g_hwndPlot=NULL;
+HBRUSH g_hbrBackground = CreateSolidBrush(RGB(0, 0, 0));
+Chart* g_cChart;
+
+
+double QuadraticEquation(double);
+std::vector<Chart::DotPosition<int>> g_vDots;
+
+#define CHART_WIDTH 400
+#define CHART_HEIGHT 400
+#define RESOLUTION 400
